8499eec2846a31a68e280cbaf7333211835d7700
whazzabii7 <whazzabii7@gmail.com>
2026-05-08 20:21:54 +0200

    removed Status

--- internal/models/models.go
+++ internal/models/models.go
@@ -6,26 +6,35 @@ import (
 
 // BotBlueprint repräsentiert die installierte Binary (der Bauplan)
 type BotBlueprint struct {
-	Alias    string    `json:"alias"`
-	Path     string    `json:"path"`
-	Type     string    `json:"type"`
-	LastScan time.Time `json:"last_scan"`
+	Alias       string    `json:"alias"`
+	Path        string    `json:"path"`
+	Type        string    `json:"type"`
+	Version     string    `json:"version"`
+	Description string    `json:"description"`
+	LastScan    time.Time `json:"last_scan"`
 }
 
 // BotInstance repräsentiert einen laufenden Prozess basierend auf einem Blueprint
 type BotInstance struct {
-	ID       int       `json:"id"`
-	Alias    string    `json:"alias"` // Dies ist logisch dein Foreign Key zum Blueprint
-	PID      int       `json:"pid"`
-	Status   string    `json:"status"`    // starting, active, zombie, dead
-	LastSeen time.Time `json:"last_seen"`
+	ID          int       `json:"id"`
+	Alias       string    `json:"alias"` // Dies ist logisch dein Foreign Key zum Blueprint
+	PID         int       `json:"pid"`
+	Status      string    `json:"status"`    // starting, active, zombie, dead
+	LastSeen    time.Time `json:"last_seen"`
 }
 
 // Task repräsentiert einen konkreten Auftrag für einen Bot
 type Task struct {
-	ID        string    `json:"id"` // Tipp: Nutze hier String, falls du UUIDs verwenden willst
-	TargetBot string    `json:"target_bot"` // Logischer Foreign Key zu BotBlueprint.Alias
-	Payload   string    `json:"payload"`
-	Status    string    `json:"status"` // pending, processing, done, failed
-	CreatedAt time.Time `json:"created_at"`
+	ID          string    `json:"id"` // Tipp: Nutze hier String, falls du UUIDs verwenden willst
+	TargetBot   string    `json:"target_bot"` // Logischer Foreign Key zu BotBlueprint.Alias
+	Payload     string    `json:"payload"`
+	Status      string    `json:"status"` // pending, processing, done, failed
+	CreatedAt   time.Time `json:"created_at"`
+}
+
+// Mainframe Requests
+type MFRequest struct {
+	RequestType int      `json:"request_type"`
+	Payload     any      `json:"payload"`
+	ResultChan  chan any `json:"requsted_data"`
 }
