f696803e150b759c01d76ec7df897335cec79398
whazzabii7 <whazzabii7@gmail.com>
2026-05-08 20:26:50 +0200

    improved and out sourced functions

--- internal/db/guardian.go
+++ internal/db/guardian.go
@@ -12,11 +12,13 @@ const (
 	SaveBlueprint RequestType = iota
 	UpdateBotStatus
 	GetActiveTasks
+	RegisterInstance
 )
 
 type DBRequest struct {
 	Type RequestType
-	Data interface{}
+	Data any
+	Response chan string 
 }
 
 type Guardian struct {
@@ -30,13 +32,31 @@ func NewGuardian() *Guardian {
 }
 
 func (g *Guardian) Start() {
+	fmt.Println("[DB-Guardian] Startup finished. Ready for requests...")
 
+	for req := range g.requestChan {
+		switch req.Type {
+		case SaveBlueprint:
+			bp, ok := req.Data.(models.BotBlueprint)
+			if ok {
+				g.processSaveBlueprint(bp)
+			}
+		case RegisterInstance:
+			bi, ok := req.Data.(models.BotInstance)
+			if ok {
+				g.handleRegisterInstance(bi)
+			}
+		default:
+			fmt.Println("[DB-Guardian] Unknown request type recieved")
+		}
+	}
 }
 
-func (g *Guardian) processSaveBlueprint(bp models.BotBlueprint) {
-
+func (g *Guardian) Stop() {
+	close(g.requestChan)
+	fmt.Println("[Guardian] Stopped.")
 }
 
-func (g *Guardian) Puplish(t RequestType, data interface{}) {
-
+func (g *Guardian) Publish(t RequestType, data any) {
+	g.requestChan <- DBRequest{Type: t, Data: data, Response: nil}
 }
