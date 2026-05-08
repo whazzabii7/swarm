561015b31c7df7eaaf3b7875bc9d8a8c9f4fba5b
whazzabii7 <whazzabii7@gmail.com>
2026-05-08 20:25:47 +0200

    added; active bot table functions

---
+++ internal/db/bot_instances.go
@@ -0,0 +1,21 @@
+package db
+
+import (
+	"log"
+	"time"
+
+	"github.com/whazzabii7/swarm/internal/models"
+)
+
+func (g *Guardian) handleRegisterInstance(inst models.BotInstance) {
+	query := `
+	INSERT INTO bot_instances (alias, pid, status, last_seen)
+	VALUES (?, ?, ?, ?);`
+
+	_, err := DB.Exec(query, inst.Alias, inst.PID, inst.Status, inst.LastSeen.Format(time.RFC3339))
+	if err != nil {
+		log.Printf("[-] Guardian Error: Could not register instance for %s: %v", inst.Alias, err)
+		return
+	}
+	log.Printf("[+] Guardian: Instance of '%s' registered with PID %d", inst.Alias, inst.PID)
+}
