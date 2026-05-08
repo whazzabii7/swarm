dcba29b07b6e0ef4baa6e826fcf69a0e2f60301d
whazzabii7 <whazzabii7@gmail.com>
2026-05-08 20:25:27 +0200

    added; blueprint table functions

---
+++ internal/db/bot_blueprints.go
@@ -0,0 +1,29 @@
+package db
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/whazzabii7/swarm/internal/models"
+)
+
+func (g *Guardian) processSaveBlueprint(bp models.BotBlueprint) {
+	fmt.Printf("[DB-Guardian] SQL-Action: Saving Bot '%s' (%s)\n", bp.Alias, bp.Path)
+	query := `
+	INSERT INTO bot_blueprints (alias, path, type, version, description, last_scan)
+	VALUES (?, ?, ?, ?, ?, ?)
+	ON CONFLICT(alias) DO UPDATE SET
+			path = excluded.path,
+			type = excluded.type,
+			version = excluded.version,
+			description = excluded.description,
+			last_scan = excluded.last_scan;`
+
+	_, err := DB.Exec(query, bp.Alias, bp.Path, bp.Type, bp.Version, bp.Description, bp.LastScan.Format(time.RFC3339))
+	if err != nil {
+		log.Printf("[-] Guardian Error with SQL-Upsert for %s: %v", bp.Alias, err)
+		return
+	}
+	log.Printf("[+] Guardian Blueprint '%s' syncronised.", bp.Alias)
+}
