39974b0f88d0dbda89eb67f28cbbe60d138a3510
whazzabii7 <whazzabii7@gmail.com>
2026-05-11 11:13:29 +0200

    changed to fit new command patterns

--- internal/mf/mainframe.go
+++ internal/mf/mainframe.go
@@ -1,7 +1,8 @@
 package mf
 
 import (
-	_ "fmt"
+	"context"
+	// "fmt"
 	"log"
 	"os"
 	"time"
@@ -13,58 +14,68 @@ import (
 	"github.com/whazzabii7/swarm/internal/models"
 )
 
+
 type Mainframe struct {
+	dbpath string
+	requestChan chan models.Request[models.MFRequest]
+
+	// Submodules
 	guardian *db.Guardian
 	manager *bot.BotManager
 	tasker  *tasker.TaskManager
 	cmder   *CommandParser
-	dbpath string
 
 	// RAM-Memory (State)
 	blueprints map[string]models.BotBlueprint // Key: Alias
 	instances  map[int]models.BotInstance     // Key: PID
-
-	requestChan chan models.MFRequest
 }
 
 func NewMainframe() *Mainframe {
-	// scan for database
+	// scan for database directory
 	if _, err := os.Stat("data"); os.IsNotExist(err) {
 		os.Mkdir("data", 0755)
 	}
 
+	// initializing request channel and Mainframe "RAM"
+	// base initialization needed for submodules
 	m := Mainframe{
-		requestChan: make(chan models.MFRequest, 100),
+		requestChan: make(chan models.Request[models.MFRequest], 100),
 		dbpath: "./data/swarm.db",
 		blueprints: make(map[string]models.BotBlueprint),
 		instances: make(map[int]models.BotInstance),
 	}
+
 	m.guardian = db.NewGuardian()
 	m.manager = bot.NewBotManager(m.requestChan)
 	m.tasker = tasker.NewTaskManager(m.requestChan)
-	m.cmder = NewComandParser(m.requestChan)
+	m.cmder = NewComandParser()
 	return &m
 }
 
 func (m *Mainframe) Start(done chan bool) {
+	ctx, cancel := context.WithCancel(context.Background())
+
 	// DB initializing
-	db.InitDB(m.dbpath)
-	go m.guardian.Start()
+	db.InitDB(ctx, m.dbpath)
 
-	go m.manager.Start()
+	// Submodule initializing
+	isStarted := make(chan bool)
 
+	go m.guardian.Start(ctx, isStarted)
+	m.wait(isStarted)
+	go m.manager.Start(ctx, isStarted)
+	m.wait(isStarted)
 	go m.cmder.RunShell()
 
 	// main loop
 	log.Println("[!] Mainframe running. Waiting for instructions...")
-
 	for {
 		select {
 			case req := <-m.requestChan:
 				m.handleRequest(req)
-			case cmd := <-m.cmder.CommandRequest:
-				if cmd.Commandtype == CmdQuit {
-					m.shutdown(done)
+			case cmd := <-m.cmder.CommandChan:
+				if cmd.Type == CmdQuit {
+					m.shutdown(done, cancel)
 					return
 				}
 				m.executeCommand(cmd)
@@ -74,13 +85,19 @@ func (m *Mainframe) Start(done chan bool) {
 	}
 }
 
-func (m *Mainframe) handleRequest(req models.MFRequest) {}
+func (m *Mainframe) wait(cond chan bool) { 
+	if <-cond {
+		return
+	}
+}
+func (m *Mainframe) handleRequest(req models.Request[models.MFRequest]) {}
 func (m *Mainframe) executeCommand(cmd Command) {}
 func (m *Mainframe) checkHealth() {}
 
-func (m *Mainframe) shutdown(done chan bool) {
-	m.guardian.Stop()
-	m.manager.Stop()
+func (m *Mainframe) shutdown(done chan bool, cancel context.CancelFunc) {
+	cancel()
 	m.tasker.Stop()
+	m.manager.Stop()
+	m.guardian.Stop()
 	done<-true
 }
