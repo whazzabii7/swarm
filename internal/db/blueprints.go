d3072906d2cb403336b0c1cd11067ef19824574e
whazzabii7 <whazzabii7@gmail.com>
2026-05-24 20:15:39 +0200

    updated RPR-System; finished scan Blueprints functions

--- internal/db/blueprints.go
+++ internal/db/blueprints.go
@@ -1,14 +1,15 @@
 package db
 
 import (
-    "context"
+	"context"
+	"fmt"
 	"time"
 
 	"github.com/whazzabii7/swarm/internal/models"
 	"github.com/whazzabii7/swarm/internal/ui"
 )
 
-func (g *Guardian) processSaveBlueprint(ctx context.Context, bp models.BotBlueprint) {
+func (g *Guardian) processSaveBlueprint(ctx context.Context, bp models.BotBlueprint) error {
 	ui.Logf(ui.LevelInfo, "DB-Guardian", "SQL-Action: Saving Bot '%s' (%s)\n", bp.Alias, bp.Path)
 	query := `
 	INSERT INTO bot_blueprints (alias, path, type, version, description, last_scan)
@@ -22,13 +23,13 @@ func (g *Guardian) processSaveBlueprint(ctx context.Context, bp models.BotBluepr
 
 	_, err := DB.ExecContext(ctx, query, bp.Alias, bp.Path, bp.Type, bp.Version, bp.Description, bp.LastScan.Format(time.RFC3339))
 	if err != nil {
-		ui.Logf(ui.LevelError, "DB-Guardian", "[-] Error with SQL-Upsert for %s: %v", bp.Alias, err)
-		return
+		return fmt.Errorf("%w for %s: %v", ErrSQLUsert, bp.Alias, err)
 	}
 	ui.Logf(ui.LevelInfo, "DB-Guardian", "[+] Blueprint '%s' syncronised.", bp.Alias)
+	return nil
 }
 
-func (g *Guardian) handleGetBlueprint(ctx context.Context, bpAlias string, response chan models.Response) {
+func (g *Guardian) handleGetBlueprint(ctx context.Context, bpAlias string) (*models.BotBlueprint, error) {
 	ui.Logf(ui.LevelInfo, "DB-Guardian", "SQL-Action: Get Bot '%s'\n", bpAlias)
 	query := `
 	SELECT alias, path, type, version, description, last_scan
@@ -36,25 +37,23 @@ func (g *Guardian) handleGetBlueprint(ctx context.Context, bpAlias string, respo
     WHERE alias = ?
     LIMIT 1;`
 
-    var bp models.BotBlueprint
+	var bp models.BotBlueprint
 
-    err := DB.QueryRowContext(ctx, query, bpAlias).Scan(
-        &bp.Alias,
-        &bp.Path,
-        &bp.Type,
-        &bp.Version,
-        &bp.Description,
-        &bp.LastScan,
-    )
-    if err != nil {
-		ui.Logf(ui.LevelError, "DB-Guardian", "[-] Error with SQL-Upsert for %s: %v", bp.Alias, err)
-        response <- models.Response{ Err: err, Payload: models.Payload(nil)  }
-        return
-    }
+	err := DB.QueryRowContext(ctx, query, bpAlias).Scan(
+		&bp.Alias,
+		&bp.Path,
+		&bp.Type,
+		&bp.Version,
+		&bp.Description,
+		&bp.LastScan,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("%w for %s: %v", ErrSQLUsert, bp.Alias, err)
+	}
 	ui.Logf(ui.LevelInfo, "DB-Guardian", "[+] Blueprint '%s' loaded.", bp.Alias)
-    response <- models.Response { Err: nil, Payload: models.Payload(bp) }
+	return &bp, nil
 }
 
-func (g *Guardian) handleCheckBlueprints(ctx context.Context, bps []models.BotBlueprint) {
-
+func (g *Guardian) handleCheckBlueprints(ctx context.Context, bps []models.BotBlueprint) ([]models.BotBlueprint, error) {
+	return nil, nil
 }
--- internal/db/guardian.go
+++ internal/db/guardian.go
@@ -2,7 +2,7 @@ package db
 
 import (
 	"context"
-	
+
 	"github.com/whazzabii7/swarm/internal/models"
 	"github.com/whazzabii7/swarm/internal/ui"
 )
@@ -30,25 +30,28 @@ func NewGuardian() *Guardian {
 
 func (g *Guardian) Start(ctx context.Context, isStarted chan bool) {
 	ui.Log(ui.LevelInfo, "Guardian", "Startup finished. Ready for requests...")
-	isStarted<-true
+	isStarted <- true
 
 	for req := range g.requestChan {
+		ui.Logf(ui.LevelInfo, "DB-Guardian", "Recieved Request: %v", req)
 		switch req.Type {
 		case DBSaveBlueprint:
-		    if bp, ok := req.Payload.GetBluePrint(); ok {
-				g.processSaveBlueprint(ctx, bp)
+			if getBlueprint, ok := models.UnwrapPayload[func() models.BotBlueprint](req.Payload); ok {
+				err := g.processSaveBlueprint(ctx, getBlueprint())
+				models.NewResponseErr(err).Submit(req.Response)
 			}
 		case DBGetBlueprint:
-			if bp, ok := req.Payload.GetString(); ok {
-				g.handleGetBlueprint(ctx, bp, req.Response)
+			if getBlueprintAlias, ok := models.UnwrapPayload[func() string](req.Payload); ok {
+				blueprint, err := g.handleGetBlueprint(ctx, getBlueprintAlias())
+				models.NewResponse[models.BotBlueprint](*blueprint, err).Submit(req.Response)
 			}
 		case DBCheckBlueprints:
-			if bps, ok := req.Payload.GetBluePrints(); ok {
-				g.handleCheckBlueprints(ctx, bps)
+			if getBlueprints, ok := models.UnwrapPayload[func() []models.BotBlueprint](req.Payload); ok {
+				g.handleCheckBlueprints(ctx, getBlueprints())
 			}
 		case DBRegisterInstance:
-			if bi, ok := req.Payload.GetInstance(); ok {
-				g.handleRegisterInstance(ctx, bi)
+			if getInstance, ok := models.UnwrapPayload[func() models.BotInstance](req.Payload); ok {
+				g.handleRegisterInstance(ctx, getInstance())
 			}
 		default:
 			ui.Log(ui.LevelInfo, "Guardian", "Unknown request type recieved")
@@ -63,5 +66,5 @@ func (g *Guardian) Stop(isStopped chan bool) {
 }
 
 func (g *Guardian) Submit(t DBRequest, data any, response chan models.Response) {
-	g.requestChan <- models.Request[DBRequest]{Type: t, Payload: models.Payload(data), Response: response}
+	g.requestChan <- models.NewRequest[DBRequest](t, data, response)
 }
